39b901a7db1fa6ac2562bdf8db0920377879f937
yangyiko <yangyiko@163.com>
2021-01-17 20:01:28 +0800

    多线程

--- src/example/按钮_呼吸灯.cpp
+++ src/example/按钮_呼吸灯.cpp
@@ -1,17 +1,12 @@
-// #include <Arduino.h>
-
 // /**********************************************************************
 // 项目名称/Project          : 零基础入门学用物联网
-// 程序名称/Program name     : c_parameter
+// 程序名称/Program name     : d_multi_ms
 // 团队/Team                : 太极创客团队 / Taichi-Maker (www.taichi-maker.com)
-// 作者/Author            
-//   : CYNO朔
+// 作者/Author              : CYNO朔
 // 日期/Date（YYYYMMDD）     : 20200703
 // 程序目的/Purpose          : 
-// 本程序旨在演示如何向Ticker库定时调用函数来传递参数。
-// 请注意：attach函数所能传递的参数最多只有一个。
-// 另外该参数仅能是这些类型中的一种：char, short, int, float, void*, char*
- 
+// 本程序旨在演示如何使用多个Ticker对象来让ESP8266处理多个任务。
+
 // 如需了解本程序的详细说明，请参考以下函数：
 // http://www.taichi-maker.com/homepage/esp8266-nodemcu-iot/iot-c/esp8266-tips/tinker/
 // -----------------------------------------------------------------------
@@ -22,41 +17,50 @@
 // ***********************************************************************/
 // #include <Ticker.h>
 // #include <analogWrite.h>
- 
+
 // Ticker ticker;
- 
+// Ticker buttonTicker;
+
 // int count;
 
 // void sayHi(int hiTimes){
 //   count++;
 //   Serial.print("Hi ");
 //   Serial.println(count);
- 
+
 //   if (count >= hiTimes) {
 //     ticker.detach();
 //     Serial.print("ticker.detach();");
 //   }
-
-//     for (int fadeValue = 0 ; fadeValue <= 1023; fadeValue += 5) {
-//     analogWrite(BUILTIN_LED, fadeValue);
-//     delay(10);
-//   }
- 
-//   for (int fadeValue = 1023 ; fadeValue >= 0; fadeValue -= 5) {
-//     analogWrite(BUILTIN_LED, fadeValue);
-//     delay(10);
-//   }
-//   // delay(3000);
 // }
- 
+
+
+// void buttonCheck(){
+//   if (digitalRead(KEY_BUILTIN) == LOW){
+//     Serial.println("KEY_BUILTIN Button Pushed...");
+//   }
+// }
+
 // void setup() {
 //   Serial.begin(9600);
-//   pinMode(BUILTIN_LED, OUTPUT);
+//   pinMode(LED_BUILTIN, OUTPUT);
+//   pinMode(KEY_BUILTIN, INPUT_PULLUP);
   
-//   ticker.attach(1, sayHi, 8);
+//   ticker.attach(1, sayHi, 60);
+//   buttonTicker.attach_ms(100, buttonCheck);
 // }
- 
-// void loop() {
 
+// void loop() {
+//   for (int fadeValue = 0 ; fadeValue <= 1023; fadeValue += 5) {
+//     analogWrite(LED_BUILTIN, fadeValue);
+//     delay(10);
+//   }
+
+//   for (int fadeValue = 1023 ; fadeValue >= 0; fadeValue -= 5) {
+//     analogWrite(LED_BUILTIN, fadeValue);
+//     delay(10);
+//   }
+//   delay(3000);
 // }
- 
+
+
